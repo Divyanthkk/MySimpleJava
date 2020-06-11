8d7af5d7ead75ed0e15e61eb3172aa452f07969b
divyanth <divyanthkalaiyarasan@gmail.com>
2020-06-11 12:43:09 +0530

    commiting the simple java file

---
+++ SimpleHello/src/com/pack/hello/display.java
@@ -0,0 +1,12 @@
+package com.pack.hello;
+
+public class display {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello hello=new Hello("Welcome !!!!");
+		
+	System.out.println(hello.getName());
+	}
+
+}
